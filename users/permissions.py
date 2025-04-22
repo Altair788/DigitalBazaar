db3f3d3689a1fafec5064f4fed3e5a2ff7ccaeae
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-04-22 16:27:12 +0300

    update: add permisson CanViewAPI

--- users/permissions.py
+++ users/permissions.py
@@ -1,30 +1,30 @@
 from rest_framework import permissions
 
+class CanViewAPI(permissions.BasePermission):
+    """
+    Разрешение для просмотра API только активным сотрудникам.
+    """
+    # комментарий к статусу is_active - user становится активным при подтверждении email
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.is_staff)
+
+
 #
-# class CanViewAPI(permissions.BasePermission):
+# class IsAdmin(permissions.BasePermission):
 #     """
-#     Разрешение для просмотра API только администраторам.
+#     Проверяет, является ли пользователь администратором.
 #     """
 #
 #     def has_permission(self, request, view):
-#         return request.user.is_staff
-
-
-class IsAdmin(permissions.BasePermission):
-    """
-    Проверяет, является ли пользователь администратором.
-    """
-
-    def has_permission(self, request, view):
-        return request.user.groups.filter(name="admins").exists()
-
-
-class IsAuthor(permissions.BasePermission):
-    """
-    Проверяет, является ли пользователь владельцем.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        if obj.author == request.user:
-            return True
-        return False
+#         return request.user.groups.filter(name="admins").exists()
+#
+#
+# class IsAuthor(permissions.BasePermission):
+#     """
+#     Проверяет, является ли пользователь владельцем.
+#     """
+#
+#     def has_object_permission(self, request, view, obj):
+#         if obj.author == request.user:
+#             return True
+#         return False
