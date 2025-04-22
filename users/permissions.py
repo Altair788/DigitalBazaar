3327272232a4b71c0d1763dfedcd266918fd8f42
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-04-23 00:34:01 +0300

    fixed: add a check on is_staff

--- users/permissions.py
+++ users/permissions.py
@@ -1,11 +1,9 @@
 from rest_framework import permissions
 
 # users/permissions.py
-# предполагается, что регистрируются в сервисе сотрудники компании
-# (активными сотрудниками они становятся при подтверждении почты) Аутентификация проходит по email
 class IsActiveEmployee(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_active
+        return request.user.is_authenticated and request.user.is_active and request.user.is_staff
 
 
 
