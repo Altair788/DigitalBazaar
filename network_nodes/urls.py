587a38bf79e2a14510d2cea8e68e82bf53290860
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-04-22 14:00:59 +0300

    feat: add urls for CRUD NetworkNodes

---
+++ network_nodes/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+
+
+from network_nodes.apps import NetworkNodesConfig
+from network_nodes.views import NetworkNodeListAPIView, NetworkNodeCreateAPIView, NetworkNodeRetrieveAPIView, \
+    NetworkNodeUpdateAPIView, NetworkNodeDestroyAPIView
+
+app_name = NetworkNodesConfig.name
+
+urlpatterns = [
+    path("", NetworkNodeListAPIView.as_view(), name="network-nodes-list"),
+    path("create/", NetworkNodeCreateAPIView.as_view(), name="network-nodes-create"),
+    path("<int:pk>/", NetworkNodeRetrieveAPIView.as_view(), name="network-nodes-retrieve"),
+    path("update/<int:pk>/", NetworkNodeUpdateAPIView.as_view(), name="network-nodes-update"),
+    path(
+        "delete/<int:pk>/",
+        NetworkNodeDestroyAPIView.as_view(),
+        name="network-nodes-delete",
+    ),
+]
