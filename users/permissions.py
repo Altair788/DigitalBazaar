da6387b941e86ce49a773c404e359963ada4b7a7
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-04-22 23:14:28 +0300

    update: add IsActiveEmployee permission

--- users/permissions.py
+++ users/permissions.py
@@ -1,30 +1,18 @@
 from rest_framework import permissions
 
-class CanViewAPI(permissions.BasePermission):
-    """
-    Разрешение для просмотра API только активным сотрудникам.
-    """
-    # комментарий к статусу is_active - user становится активным при подтверждении email
+# users/permissions.py
+# предполагается, что регистрируются в сервисе сотрудники компании
+# (активными сотрудниками они становятся при подтверждении почты) Аутентификация проходит по email
+class IsActiveEmployee(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.is_staff)
+        return request.user.is_authenticated and request.user.is_active
 
 
-#
-# class IsAdmin(permissions.BasePermission):
-#     """
-#     Проверяет, является ли пользователь администратором.
-#     """
-#
-#     def has_permission(self, request, view):
-#         return request.user.groups.filter(name="admins").exists()
-#
-#
-# class IsAuthor(permissions.BasePermission):
-#     """
-#     Проверяет, является ли пользователь владельцем.
-#     """
-#
-#     def has_object_permission(self, request, view, obj):
-#         if obj.author == request.user:
-#             return True
-#         return False
+
+class IsAdmin(permissions.BasePermission):
+    """
+    Проверяет, является ли пользователь администратором.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="admins").exists()
