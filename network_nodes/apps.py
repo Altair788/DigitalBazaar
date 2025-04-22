cb47dd9c5824a45f7953417c653ffe334de89b2a
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-04-22 14:05:09 +0300

    update: add verbose_name

---
+++ network_nodes/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NetworkNodesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "network_nodes"
+    verbose_name = "Network_nodes"
